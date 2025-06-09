610b1266443979f9ac911277370d8f0399333d07
suryavanshironak1 <suryavanshironak1@gmail.com>
2025-06-10 00:07:53 +0530

    Update UserController.java

--- spring-sec-demo/src/main/java/com/telusko/spring_sec_demo/controller/UserController.java
+++ spring-sec-demo/src/main/java/com/telusko/spring_sec_demo/controller/UserController.java
@@ -3,7 +3,7 @@ package com.telusko.spring_sec_demo.controller;
 import com.telusko.spring_sec_demo.model.User;
 import com.telusko.spring_sec_demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +13,7 @@ public class UserController {
     @Autowired
     private UserService service;
 
-    @PutMapping("register")
+    @PostMapping("register")
     public User register(@RequestBody User user){
         System.out.println("register called");
         return service.saveUser(user);
